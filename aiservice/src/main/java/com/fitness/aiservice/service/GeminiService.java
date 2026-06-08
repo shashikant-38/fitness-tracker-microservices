df457e8b466bb49b602e2b9efb2aafe1bf4d9ade
shashikant-38 <patilshashikant4508@gmail.com>
2026-06-08 23:54:08 +0530

    Added Gemini service integration and updated AI service configuration

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,44 @@
+package com.fitness.aiservice.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GeminiService {
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getRecommendations(String Details){
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text",Details)
+                        })
+                }
+        );
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+//api call using resonse obj
+        return response;
+    }
+}
