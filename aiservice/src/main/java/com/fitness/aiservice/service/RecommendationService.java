fe9fa77bdcd78d84400d7b3b1d2a76e57f2ddacd
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-10 22:30:03 +0530

    update ai service

--- aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -10,12 +10,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
-    private final RecommendationRepository recommendationRepository;
+     private final RecommendationRepository recommendationRepository;
 
     public List<Recommendation> getUserRecommendation(String userId) {
         return recommendationRepository.findByUserId(userId);
     }
-
     public Recommendation getActivityRecommendation(String activityId) {
         return recommendationRepository.findByActivityId(activityId)
                 .orElseThrow(() -> new RuntimeException("No recommendation found for this activity: " + activityId));
