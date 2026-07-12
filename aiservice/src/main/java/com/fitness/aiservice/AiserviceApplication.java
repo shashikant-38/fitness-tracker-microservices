9225e3ceb4ce9bc7823adab321b51d76e347409f
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-12 22:48:25 +0530

    Update service

--- aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AiserviceApplication {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		SpringApplication.run(AiserviceApplication.class, args);
 	}
 
