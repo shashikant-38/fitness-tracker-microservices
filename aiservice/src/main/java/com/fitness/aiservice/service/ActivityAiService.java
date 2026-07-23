0e96d8430aafe1bb95f5b8e0e5c3d3255452b028
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-23 16:04:20 +0530

    Recommendation update

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityAiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAiService.java
@@ -9,7 +9,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.LifecycleState;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.stereotype.Service;
+
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -42,11 +49,50 @@ public class ActivityAiService {
                         .trim();
 //                log.info("RESPONSE FROM CLEANED AI {}", jsonContent);
 
+                JsonNode analysisJson =mapper.readTree(jsonContent);
+                JsonNode analysisNode = analysisJson.path("analysis");
+                StringBuilder fullAnalysis=new StringBuilder();
+                addAnalysisSecction(fullAnalysis,analysisNode,"overall","Overall:");
+                addAnalysisSecction(fullAnalysis,analysisNode,"pace","Pace:");
+                addAnalysisSecction(fullAnalysis,analysisNode,"heartRate","Heart Rate:");
+                addAnalysisSecction(fullAnalysis,analysisNode,"caloriesBurned","Calories:");
+
+                List<String> improvements= extractImprovements (analysisJson.path("improvements"));
+                List<String> suggestion= extractSuggestions (analysisJson.path("suggestions"));
+                List<String> safety= extractSafety (analysisJson.path("Safety"));
 
             }catch(Exception e){
 
             }
             return null;
+    }
+
+    private List<String> extractSafety(JsonNode safety) {
+    }
+
+    private List<String> extractSuggestions(JsonNode suggestions) {
+    }
+
+    private List<String> extractImprovements(JsonNode improvementsNode) {
+        List<String> improvements = new ArrayList<>();
+        if(improvementsNode.isArray()){
+            improvementsNode.forEach(improvement ->{
+                String area =improvement.path("area").asText();
+                String detail =improvement.path("recommendation ").asText();
+                improvements.add(String.format("%s: %s",area,detail));
+            });
+        }
+        return improvements.isEmpty() ?
+                Collections.singletonList("No specific improvements provided"):
+                improvements;
+    }
+
+    private void addAnalysisSecction(StringBuilder fullAnalysis, JsonNode analysisNode, String key, String prefix) {
+        if(!analysisNode.path(key).isMissingNode()){
+            fullAnalysis.append(prefix)
+                    .append(analysisNode.path(key).asText())
+                    .append("\n\n");
+        }
     }
 
     //prompt FOR AI
