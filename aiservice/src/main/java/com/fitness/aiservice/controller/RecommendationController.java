1126f53c74f2223574046d1b03bfef81f6421648
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-28 00:10:18 +0530

    update controller

--- aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -22,5 +22,8 @@ public class RecommendationController {
         return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
     }
 
-
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId) {
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
 }
