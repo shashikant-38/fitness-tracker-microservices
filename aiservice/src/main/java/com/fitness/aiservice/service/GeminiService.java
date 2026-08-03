6ed55edb456f377e7b37ff7cee0caa0d958e1ef0
shashikant-38 <patilshashikant4508@gmail.com>
2026-08-03 21:57:04 +0530

    update gemini service

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,5 +1,8 @@
 package com.fitness.aiservice.service;
 
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,37 +10,37 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
 
 @Service
+@Data
 public class GeminiService {
-
     private final WebClient webClient;
 
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
 
-    @Value("${gemini.api.key}")
-    private String geminiApiKey;
+     @Value("${gemini.api.key}" )
+    private String geminiApiKey ;
 
-    public GeminiService(WebClient.Builder webClientBuilder) {
+
+    public GeminiService(WebClient.Builder webClientBuilder){
         this.webClient = webClientBuilder.build();
     }
 
-    public String getAnswer(String question) {
-        Map<String, Object> requestBody = Map.of(
-                "contents", new Object[] {
+    public String getRecommendations(String Details){
+        Map<String,Object> requestBody = Map.of(
+                "contents",new Object[]{
                         Map.of("parts", new Object[]{
-                                Map.of("text", question)
+                                Map.of("text",Details)
                         })
                 }
         );
-
         String response = webClient.post()
-                .uri(geminiApiUrl + geminiApiKey)
+                .uri(geminiApiUrl + "?key="  + geminiApiKey)
                 .header("Content-Type", "application/json")
                 .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(String.class)
-                .block();
-
+                .block() ;
+//api call using  resonse obj
         return response;
     }
 }
