80154953abc8284d623e2bb33e95475feb5df5a2
shashikant-38 <patilshashikant4508@gmail.com>
2026-08-13 23:57:08 +0530

    auth addded

--- gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -1,10 +1,22 @@
 package com.fitness.gateway;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
 @EnableWebFlux
 public class SecurityConfig {
+    @Bean
+    public class SecurityFilterChain{
+    public SecurityFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
+                .build();
+}
 }
